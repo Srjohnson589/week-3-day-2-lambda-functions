ab8b2c14d269e4af34077b0c35040f074f1c812d
Srjohnson589 <srjohnson4586@gmail.com>
2024-01-23 21:28:46 -0500

    first commit

---
+++ whiteboard.py
@@ -0,0 +1,24 @@
+
+
+# ---------- Reversed Strings ----------
+# DESCRIPTION:
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'thieves'   =>  'seveiht'
+
+#write a function that takes a string
+def reverse_str(str):
+
+#reverse
+    return str[::-1]
+
+# return the reversed string
+
+print(reverse_str('world'))
+
+
+
+
+
+
+
